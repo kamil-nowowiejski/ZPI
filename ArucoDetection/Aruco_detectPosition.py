4658c0a4d7b93c491460816b9622cb5478e19f8b
LordSkin <bartek.kramnik@wp.pl>
2017-06-06 17:38:22 +0200

    Rozpoznawanie aruco w pełni sprawne

--- ArucoDetection/Aruco_detectPosition.py
+++ ArucoDetection/Aruco_detectPosition.py
@@ -1,65 +1,53 @@
 import cv2.aruco as aruco
 import yaml
-import numpy.linalg.norm as norm
 import numpy as np
 import math
-
+import cv2
 import numpy
 
 
 class MrkerDetector:
 
     markerSize = 0.05
-    camera_matrix = None
-    dist_coeffs = None
-    aruco_dict = None
-    parameters = None
+    _camera_matrix = None
+    _dist_coeffs = None
+    _aruco_dict = None
+    _parameters = None
 
     def b_setUp(self):
-        calibrationFile = "calibration.yaml"
+        calibrationFile = "calibration_raspberry.yaml"
 
         try:
             plik = open(calibrationFile, "r")
             myYaml = yaml.load(plik)
-            self.camera_matrix = numpy.asanyarray(myYaml.get("camera_matrix"))
-            self.dist_coeffs = numpy.asanyarray(myYaml.get("dist_coeff"))
-            self.aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
-            self.parameters = aruco.DetectorParameters_create()
+            self._camera_matrix = numpy.asanyarray(myYaml.get("camera_matrix"))
+            self._dist_coeffs = numpy.asanyarray(myYaml.get("dist_coeff"))
+            self._aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+            self._parameters = aruco.DetectorParameters_create()
             return True
         except:
             return False
 
     def detect(self, image):
-        corners, ids, rejectedImgPoints = aruco.detectMarkers(image, self.aruco_dict, parameters=self.parameters)
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(image, self._aruco_dict, parameters=self._parameters)
         if corners:
-            rvecs, tvecs, objectPoints = aruco.estimatePoseSingleMarkers(corners, self.markerSize, cameraMatrix=self.camera_matrix,distCoeffs=self.dist_coeffs)
+            rvecs, tvecs, objectPoints = aruco.estimatePoseSingleMarkers(corners, self.markerSize, cameraMatrix=self._camera_matrix, distCoeffs=self._dist_coeffs)
+            for x in range(0, len(rvecs)):
+                rvecs[x] = self._convert_rot_matrix_to_degrees(rvecs[x])
             return rvecs, tvecs
         else: return None, None
 
+    def _convert_rot_matrix_to_degrees(self, rvec):
 
-    def _rodrigues(self, rot_vec):
-        theta = norm(rot_vec)
-        rot_vec = rot_vec / theta
+        rot_matrix = numpy.empty([3, 3])
+        cv2.Rodrigues(rvec, rot_matrix, None)
 
-        cos_theta = math.cos(theta)
-        cos_theta_matrix = np.ndarray([[cos_theta]])
-        cos_theta_matrix = np.repeat(cos_theta_matrix, 3, axis=1)
-        cos_theta_matrix = np.repeat(cos_theta_matrix, 3, axis=0)
+        x_rot = math.atan2(rot_matrix[2][1], rot_matrix[2][2])
+        y_rot = math.atan2(-rot_matrix[2][0], math.sqrt(math.pow(rot_matrix[2][1], 2) + math.pow(rot_matrix[2][2], 2)))
+        z_rot = math.atan2(rot_matrix[1][0], rot_matrix[0][0])
 
-        temp_r = np.array([rot_vec])
-        temp_r = temp_r * np.transpose(temp_r)
-        temp_r = temp_r * (1 - cos_theta)
-
-        last_matrix = np.array([
-            [0, -rot_vec[2], rot_vec[1]],
-            [rot_vec[2], 0, -rot_vec[0]],
-            [-rot_vec[1], rot_vec[0], 0]])
-        last_matrix = math.sin(theta) * last_matrix
-
-        return cos_theta_matrix + temp_r + last_matrix
-
-        rodriguez_matrix = []
-        x_rot = math.atan2(rodriguez_matrix[2][1], rodriguez_matrix[2][2])
-        y_rot = math.atan2(-rodriguez_matrix[2][0], math.sqrt(math.pow(rodriguez_matrix[2][1], 2) + math.pow(rodriguez_matrix[2][2], 2)))
-        z_rot = math.atan2(rodriguez_matrix[1][0], rodriguez_matrix[0][0])
-
+        if x_rot > 0:
+            x_rot = 180 - math.degrees(x_rot)
+        else:
+            x_rot = -180 - math.degrees(x_rot)
+        return round(x_rot), round(math.degrees(y_rot)), round(math.degrees(z_rot))
